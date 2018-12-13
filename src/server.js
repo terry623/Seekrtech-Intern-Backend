df1df4f5f86afd532d2f536f2c93e48116a2806c
terry623 <terry92516@gmail.com>
2018-12-14 00:10:28 +0800

    add cors & frontend can get data

--- src/server.js
+++ src/server.js
@@ -1,18 +1,19 @@
 const logger = require('koa-logger');
 const router = require('koa-router')();
 const Koa = require('koa');
+const cors = require('@koa/cors');
 const api = require('./api.js');
 
 const port = 8080;
 const app = new Koa();
 
+app.use(cors());
+
 app.use(logger());
 
-router.get('/', async ctx => {
-  const response = await api.getForestRankingData({
-    number: 10,
-    lastPosition: 0,
-  });
+router.get('/:number/:lastPosition', async ctx => {
+  const { number, lastPosition } = ctx.params;
+  const response = await api.getForestRankingData({ number, lastPosition });
   ctx.body = JSON.stringify(response);
 });
 
